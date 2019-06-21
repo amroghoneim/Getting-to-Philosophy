16b26565b705b656f82c3222865781f2a30c564d
Amro Ghoneim <37261960+amroghoneim@users.noreply.github.com>
2019-06-21 11:45:59 +0200

    Update gtp.py

--- gtp.py
+++ gtp.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import re
 import time
 
+def remove_text_between_parens(text):
+        n = 1  # run at least once
+        while n:
+            text, n = re.subn(r'\([<a^()]*\)', '', text)  # remove non-nested/flat balanced parts
+        return text
+
+
 history = []
 link = input("url: ")
 history.append(link)
@@ -10,27 +17,33 @@ r = requests.get(link)
 soup = BeautifulSoup(r.content, features = 'html.parser')
 
 while soup.find(id = 'firstHeading').get_text() != 'Philosophy':
+    for x in soup.find_all(['table']): # remove tables
+            x.replace_with("")
     y = soup.find_all('p', {"class" : ""})
     for i in range(len(y) ):
         for s in y[i].find_all(href = re.compile('redlink=1$')): # remove redlinks
             s.replace_with("")
         for s in y[i].find_all(['i']): # remove italics
             s.replace_with("")
-        p = str(y[i])
-        p = re.sub(r' \(.*?\)', '', p) # remove parenthesized text
-        y[i] = BeautifulSoup(p, features='html.parser') # re-convert to soup object
+        s = str(y[i])
+        s = re.sub(r'\s+\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)', '', s) # remove parenthesized text
+        y[i] = BeautifulSoup(s, 'html') # re-convert to soup object
+
 
     flag = True
+    #flag2 = True
     for i in range (len(y)): # go to paragraph that has a wiki link
-        if y[i].find(href = re.compile('^/wiki/')) != None:
+        if y[i].find(href = re.compile('/wiki/')) != None:
             content = y[i]
             flag = False # found a paragraph that has a wiki link
             break
+
     if(flag == True):
         print("no link exists!")
         break
 
     firstLink = content.find(href = re.compile('^/wiki/')) # links that start with /wiki/ only
+    
     link = 'http://en.wikipedia.org' + firstLink.get('href')
     if link in history:
         print("loop exists!")
